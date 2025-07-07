c7972569f5f157a86d30a9bf3d192ce61b92a3b8
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-07-07 21:08:28 +0900

    [test] #15 ci test용 testcode 작성

---
+++ src/test/java/org/netway/dongnehankki/CiTest.java
@@ -0,0 +1,14 @@
+package org.netway.dongnehankki;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class CiTest {
+
+	@Test
+	void ciTest(){
+		int result = 1 + 1;
+		assertEquals(2, result);
+	}
+}
