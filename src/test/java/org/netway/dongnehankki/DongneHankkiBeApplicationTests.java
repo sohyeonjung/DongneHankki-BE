c3b2c0d220562a505f2207d5380904b430e6d863
HoyiTT <pjs9177@naver.com>
2025-07-18 14:33:32 +0900

    [refactor]#15 DongneHankkiBeApplicationTests에 설정 파일 명시 추가, 임포트

--- src/test/java/org/netway/dongnehankki/DongneHankkiBeApplicationTests.java
+++ src/test/java/org/netway/dongnehankki/DongneHankkiBeApplicationTests.java
@@ -2,6 +2,7 @@ package org.netway.dongnehankki;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
