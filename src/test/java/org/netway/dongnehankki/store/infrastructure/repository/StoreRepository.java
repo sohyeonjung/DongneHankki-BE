8f852725ca98c22c4df492f3bd2ab09fc7c556ef
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-07-23 02:28:38 +0900

    [test] store repository 사업자등록번호 조회 함수 테스트 추가

--- src/test/java/org/netway/dongnehankki/store/infrastructure/repository/StoreRepository.java
+++ src/test/java/org/netway/dongnehankki/store/infrastructure/repository/StoreRepository.java
@@ -3,6 +3,7 @@ package org.netway.dongnehankki.store.infrastructure.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,25 +18,29 @@ class StoreRepositoryTest {
 	@Autowired
 	private StoreRepository storeRepository;
 
+	private Store storeA;
+	private Store storeB;
+	private Store storeC;
+
 	@BeforeEach
 	void setUp() {
 		storeRepository.deleteAll();
 
-		storeRepository.save(
+		storeA = storeRepository.save(
 			Store.createStore("가게A", 37.5665, 126.9780, "경기도 광명시 A"
 			,"경기도 광명시", "F1", "123-45-6789a0"));
 
 
-		storeRepository.save(
+		storeB = storeRepository.save(
 			Store.createStore("가게B", 37.5670, 126.9785,
 				"서울", "서울시", "F2", "098-76-54321"));
 
-		storeRepository.save(
+		storeC = storeRepository.save(
 			Store.createStore("가게C", 37.6000, 126.9800,
 				"서울", "서울시", "R1", "111-22-33333"));
 	}
 
-	@DisplayName("위도와 경도 범위 내의 가게 목록 조회")
+	@DisplayName("findByLatitudeBetweenAndLongitudeBetween - 위도와 경도 범위 내의 가게 목록 조회")
 	@Test
 	void findStoresWithinCoordinates() {
 		double minLat = 37.5660;
@@ -51,7 +56,7 @@ class StoreRepositoryTest {
 			.containsExactlyInAnyOrder("가게A", "가게B");
 	}
 
-	@DisplayName("범위 내에 가게가 없을 경우 빈 리스트를 반환")
+	@DisplayName("findByLatitudeBetweenAndLongitudeBetween - 범위 내에 가게가 없을 경우 빈 리스트를 반환")
 	@Test
 	void findStoresWhenNoneExistWithinCoordinates() {
 		double minLat = 30.0;
@@ -63,6 +68,31 @@ class StoreRepositoryTest {
 			minLat, maxLat, minLon, maxLon);
 
 		assertThat(stores).isEmpty();
+	}
+
+	@DisplayName("findByBusinessRegistrationNumber - 존재하는 사업자등록번호로 조회 성공")
+	@Test
+	void findByBusinessRegistrationNumber_existingNumber() {
+		// When
+		Optional<Store> foundStoreOptional = storeRepository.findByBusinessRegistrationNumber("098-76-54321");
+
+		// Then
+		assertThat(foundStoreOptional).isPresent();
+		assertThat(foundStoreOptional.get().getName()).isEqualTo("가게B");
+		assertThat(foundStoreOptional.get().getStoreId()).isEqualTo(storeB.getStoreId());
+	}
+
+	@DisplayName("findByBusinessRegistrationNumber - 존재하지 않는 사업자등록번호로 조회 시 빈 Optional 반환")
+	@Test
+	void findByBusinessRegistrationNumber_nonExistingNumber() {
+		// Given
+		String nonExistingNumber = "999-99-99999";
+
+		// When
+		Optional<Store> foundStoreOptional = storeRepository.findByBusinessRegistrationNumber(nonExistingNumber);
+
+		// Then
+		assertThat(foundStoreOptional).isEmpty();
 	}
 
 }
