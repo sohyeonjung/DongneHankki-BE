517d0cb658f1045f678f08e6fc458caa010c0e40
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-07-26 00:01:06 +0900

    [test] #13 industryCode, businessnumber type 바꾸기 위한 MapService test 변경

--- src/test/java/org/netway/dongnehankki/map/application/MapServiceTest.java
+++ src/test/java/org/netway/dongnehankki/map/application/MapServiceTest.java
@@ -49,23 +49,23 @@ public class MapServiceTest {
 		reset(mapBoundaryCalculator);
 		double lat_0_2km = CENTER_LAT + (0.2 / KM_PER_LATITUDE_DEGREE);
 		double lon_0_2km = CENTER_LON + (0.2 / KM_PER_LONGITUDE_DEGREE_AT_SEOUL_LATITUDE);
-		storeWithin500m = Store.createStore("500m내 가게", lat_0_2km, lon_0_2km, "주소1", "경기도 광명시", "F1", "10-111-2222");
+		storeWithin500m = Store.createStore("500m내 가게", lat_0_2km, lon_0_2km, "주소1", "경기도 광명시", 1234, 101112222L);
 
 		double lat_1_5km = CENTER_LAT + (1.5 / KM_PER_LATITUDE_DEGREE);
 		double lon_1_5km = CENTER_LON + (1.5 / KM_PER_LONGITUDE_DEGREE_AT_SEOUL_LATITUDE);
-		storeWithin2km = Store.createStore("2km내 가게", lat_1_5km, lon_1_5km, "주소2", "경기도 광명시", "F2", "10-333-4444");
+		storeWithin2km = Store.createStore("2km내 가게", lat_1_5km, lon_1_5km, "주소2", "경기도 광명시", 1234, 103334444L);
 
 		double lat_4km = CENTER_LAT + (4.0 / KM_PER_LATITUDE_DEGREE);
 		double lon_4km = CENTER_LON + (4.0 / KM_PER_LONGITUDE_DEGREE_AT_SEOUL_LATITUDE);
-		storeWithin5km = Store.createStore("5km내 가게", lat_4km, lon_4km, "주소3", "경기도 광명시", "F3", "10-555-6666");
+		storeWithin5km = Store.createStore("5km내 가게", lat_4km, lon_4km, "주소3", "경기도 광명시", 1234, 105556666L);
 
 		double lat_6km = CENTER_LAT + (6.0 / KM_PER_LATITUDE_DEGREE);
 		double lon_6km = CENTER_LON + (6.0 / KM_PER_LONGITUDE_DEGREE_AT_SEOUL_LATITUDE);
-		storeWithin7km = Store.createStore("7km내 가게", lat_6km, lon_6km, "주소4", "경기도 광명시", "F4", "10-777-8888");
+		storeWithin7km = Store.createStore("7km내 가게", lat_6km, lon_6km, "주소4", "경기도 광명시", 1235, 107778888L);
 
 		double lat_9km = CENTER_LAT + (9.0 / KM_PER_LATITUDE_DEGREE);
 		double lon_9km = CENTER_LON + (9.0 / KM_PER_LONGITUDE_DEGREE_AT_SEOUL_LATITUDE);
-		storeWithin10km = Store.createStore("10km내 가게", lat_9km, lon_9km, "주소5", "경기도 광명시", "F5", "10-999-0000");
+		storeWithin10km = Store.createStore("10km내 가게", lat_9km, lon_9km, "주소5", "경기도 광명시", 1235, 109990000L);
 
 	}
 
