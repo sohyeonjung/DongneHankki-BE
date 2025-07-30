b2fbe90ac25653f82ee7b9ac6991bb83a2133234
HoyiTT <pjs9177@naver.com>
2025-07-30 20:46:28 +0900

    [bug]#32 JpaConfig 에서 RT repo 제외

--- src/main/java/org/netway/dongnehankki/global/config/JpaConfig.java
+++ src/main/java/org/netway/dongnehankki/global/config/JpaConfig.java
@@ -1,14 +1,15 @@
 package org.netway.dongnehankki.global.config;
 
+import org.netway.dongnehankki.global.auth.jwt.RefreshTokenRepository;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = "org.netway.dongnehankki",
-	excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.netway.dongnehankki.global.auth.jwt.*Repository"))
-@Configuration
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = RefreshTokenRepository.class))
 public class JpaConfig {
 }
