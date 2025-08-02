60ea256a31f6c8042d885a8d84001f3563bd6906
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-08-02 23:00:25 +0900

    [test] #39 storeService getStore method test 추가

---
+++ src/test/java/org/netway/dongnehankki/store/application/StoreServiceTest.java
@@ -0,0 +1,59 @@
+package org.netway.dongnehankki.store.application;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.netway.dongnehankki.store.domain.Store;
+import org.netway.dongnehankki.store.exception.UnregisteredStoreException;
+import org.netway.dongnehankki.store.infrastructure.repository.StoreRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class StoreServiceTest {
+
+	@Mock
+	private StoreRepository storeRepository;
+
+	@InjectMocks
+	private StoreService storeService;
+
+	@Test
+	@DisplayName("getStore - 해당하는 store가 없을시 UnregisterException 반환")
+	void testGetStore_nullReturn() {
+		//Given
+		Long storeId = 1L;
+
+		// When
+		when(storeRepository.findById(storeId)).thenReturn(Optional.empty());
+
+		// Then
+		assertThrows(UnregisteredStoreException.class, () -> storeService.getStore(storeId));
+	}
+
+	@Test
+	@DisplayName("getStore - 해당하는 store가 있을 시 잘 반환")
+	void testProcessAndSaveStores_nullList() {
+		//Given
+		Store store = Store.createStore("storeA", 127.1535, 52.123, "경기도 광명시 A",
+			"광명시", 2316, 12356102561L);
+		Long storeId = 1L;
+
+		// When
+		when(storeRepository.findById(storeId)).thenReturn(Optional.of(store));
+		StoreResponse result = storeService.getStore(storeId);
+
+		// Then
+		assertThat(result.getName()).isEqualTo("storeA");
+		assertThat(result.getAddress()).isEqualTo("경기도 광명시 A");
+		assertThat(result.getIndustryCode()).isEqualTo(2316);
+	}
+
+}
