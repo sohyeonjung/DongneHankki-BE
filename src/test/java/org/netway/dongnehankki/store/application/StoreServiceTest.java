f4e2f60cfb2df7628c12c17a65a4c2ba3e817d45
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-08-06 21:47:26 +0900

    [test] #39 writeReview service method test

--- src/test/java/org/netway/dongnehankki/store/application/StoreServiceTest.java
+++ src/test/java/org/netway/dongnehankki/store/application/StoreServiceTest.java
@@ -12,16 +12,29 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.netway.dongnehankki.store.domain.Review;
 import org.netway.dongnehankki.store.domain.Store;
+import org.netway.dongnehankki.store.dto.request.StoreReviewRequest;
+import org.netway.dongnehankki.store.dto.response.ReviewResponse;
 import org.netway.dongnehankki.store.dto.response.StoreResponse;
 import org.netway.dongnehankki.store.exception.UnregisteredStoreException;
+import org.netway.dongnehankki.store.infrastructure.repository.ReviewRepository;
 import org.netway.dongnehankki.store.infrastructure.repository.StoreRepository;
+import org.netway.dongnehankki.user.domain.User;
+import org.netway.dongnehankki.user.exception.UnregisteredUserException;
+import org.netway.dongnehankki.user.infrastructure.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class StoreServiceTest {
 
 	@Mock
 	private StoreRepository storeRepository;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private ReviewRepository reviewRepository;
 
 	@InjectMocks
 	private StoreService storeService;
@@ -87,5 +100,65 @@ public class StoreServiceTest {
 		assertThat(result.getAddress()).isEqualTo("경기도 광명시 A");
 		assertThat(result.getIndustryCode()).isEqualTo(2316);
 	}
+
+	@Test
+	@DisplayName("writeStoreReview - 유효하지 않은 store, UnregisterException 반환 ")
+	void writeStoreReview_nullStoreReturn() {
+		// Given
+		Long storeId = 1L;
+
+		// When
+		when(storeRepository.findById(storeId)).thenReturn(Optional.empty());
+
+		// Then
+		assertThrows(UnregisteredStoreException.class, () -> storeService.writeStoreReview(storeId, null));
+	}
+
+	@Test
+	@DisplayName("writeStoreReview - 유효하지 않은 User, UnregisterException 반환 ")
+	void writeStoreReview_nullUserReturn() {
+		// Given
+		Long userId = 1L;
+		Store testStore = Store.createStore("storeA", 127.1535, 52.123, "경기도 광명시 A",
+			"광명시", 2316, 12356102561L);
+		StoreReviewRequest storeReviewRequest = StoreReviewRequest.builder().userId(1L).build();
+		// When
+		when(storeRepository.findById(any())).thenReturn(Optional.of(testStore));
+		when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+		// Then
+		assertThrows(UnregisteredUserException.class, () -> storeService.writeStoreReview(1L, storeReviewRequest));
+	}
+
+	@Test
+	@DisplayName("writeStoreReview - 유효한 입력값에서 정상 작동")
+	void writeStoreReview_success() {
+		// Given
+		Long storeId = 1L;
+		Long userId = 1L;
+		Store testStore = Store.createStore("storeA", 127.1535, 52.123, "경기도 광명시 A",
+			"광명시", 2316, 12356102561L);
+		User testUser = User.ofCustomer("1", "1234", "nickname", "name", "010-1234-5468");
+		StoreReviewRequest storeReviewRequest = StoreReviewRequest.builder().userId(1L).content("review test").scope(4).build();
+
+		// When
+		when(storeRepository.findById(storeId)).thenReturn(Optional.of(testStore));
+		when(userRepository.findById(userId)).thenReturn(Optional.of(testUser));
+
+		storeService.writeStoreReview(storeId, storeReviewRequest);
+
+		// Then
+		verify(storeRepository, times(1)).findById(storeId);
+		verify(userRepository, times(1)).findById(userId);
+		verify(reviewRepository, times(1)).save(any(Review.class));
+	}
+
+
+
+
+
+
+
+
 
 }
