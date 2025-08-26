71e34638e81dc4b2176fea61fb45db6d6d08a63b
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-08-27 00:58:54 +0900

    [feat] #61 아침, 점심, 저녁 마케팅 알림 전송 기능 추가

---
+++ src/main/java/org/netway/dongnehankki/notification/application/FCMNotificationService.java
@@ -0,0 +1,39 @@
+package org.netway.dongnehankki.notification.application;
+
+import java.util.List;
+
+import org.netway.dongnehankki.notification.dto.request.FCMNotificationRequest;
+import org.netway.dongnehankki.user.infrastructure.UserRepository;
+import org.springframework.stereotype.Service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FCMNotificationService {
+
+	public void sendFCMNotifications(String deviceToken, String title, String body) {
+		Notification notification = Notification.builder()
+			.setTitle(title)
+			.setBody(body)
+			.build();
+	
+		//TODO: key value값 프론트랑 상의해서 필요한 값으로 변경
+		Message message = Message.builder()
+			.setToken(deviceToken)
+			.setNotification(notification)
+			.putData("key", "value") 
+			.build();
+
+		try {
+			String response = FirebaseMessaging.getInstance().send(message);
+			System.out.println("Successfully sent message to token " + deviceToken + ": " + response);
+		} catch (Exception e) {
+			System.err.println("Error sending message to token " + deviceToken + ": " + e.getMessage());
+		}
+	}
+}
