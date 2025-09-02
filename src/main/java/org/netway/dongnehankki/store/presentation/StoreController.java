09260cc44825d4309a473a4ada0ce327b407ec74
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-09-02 13:32:02 +0900

    [docs] store api swagger docs 설명 추가

--- src/main/java/org/netway/dongnehankki/store/presentation/StoreController.java
+++ src/main/java/org/netway/dongnehankki/store/presentation/StoreController.java
@@ -18,9 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "가게", description = "가게 관련 API")
 @RestController
 @RequestMapping("/api/stores")
 @RequiredArgsConstructor
@@ -28,71 +32,79 @@ public class StoreController {
 
 	private final StoreService storeService;
 
+	@Operation(summary = "아아디로 가게 조회", description = "아이디를 통해 가게의 세부 정보를 조회합니다.")
 	@GetMapping("/{storeId}")
 	public ResponseEntity<ApiResponse<StoreResponse>> getStoreById(
-		@PathVariable Long storeId
+		@Parameter(description = "가게 ID") @PathVariable Long storeId
 	){
 		StoreResponse store = storeService.getStoreById(storeId);
 		return ResponseEntity.ok(ApiResponse.success(store));
 	}
 
+	@Operation(summary = "사업자번호로 가게 조회", description = "사업자번호를 통회 가게 정보를 조회합니다.")
 	@GetMapping
 	public ResponseEntity<ApiResponse<StoreResponse>> getStoreByBusinessNum(
-		@RequestParam Long businessNumber
+		@Parameter(description = "조회할 가게 사업자번호") @RequestParam Long businessNumber
 	){
 		StoreResponse store = storeService.getStoreByBusinessNum(businessNumber);
 		return ResponseEntity.ok(ApiResponse.success(store));
 	}
 
+	@Operation(summary = "가게 리뷰 작성", description = "가게 리뷰를 작성합니다.")
 	@PostMapping("/{storeId}/reviews")
 	public ResponseEntity<ApiResponse<Void>> writeStoreReview(
-		@PathVariable Long storeId,
+		@Parameter(description = "가게 ID") @PathVariable Long storeId,
 		@RequestBody @Valid CreateStoreReviewRequest createStoreReviewRequest
 	){
 		storeService.writeStoreReview(storeId, createStoreReviewRequest);
 		return ResponseEntity.ok(ApiResponse.success());
 	}
 
+	@Operation(summary = "가게 메뉴 작성", description = "가게의 메뉴를 작성합니다.")
 	@PostMapping("/{storeId}/menus")
 	public ResponseEntity<ApiResponse<Void>> addStoreMenu(
-		@PathVariable Long storeId,
+		@Parameter(description = "가게 ID") @PathVariable Long storeId,
 		@RequestBody @Valid StoreMenuRequest storeMenuRequest
 	){
 		storeService.addStoreMenu(storeId, storeMenuRequest);
 		return ResponseEntity.ok(ApiResponse.success());
 	}
 
+	@Operation(summary = "가게 메뉴 삭제", description = "가게의 메뉴를 삭제합니다.")
 	@DeleteMapping("/{storeId}/menus/{menuId}")
 	public ResponseEntity<ApiResponse<Void>> deleteStoreMenu(
-		@PathVariable Long storeId,
-		@PathVariable Long menuId
+		@Parameter(description = "가게 ID") @PathVariable Long storeId,
+		@Parameter(description = "수정할 메뉴 ID") @PathVariable Long menuId
 	){
 		storeService.deleteStoreMenu(storeId, menuId);
 		return ResponseEntity.ok(ApiResponse.success());
 	}
 
+	@Operation(summary = "가게 리뷰 삭제", description = "가게의 리뷰를 삭제합니다.")
 	@DeleteMapping("/{storeId}/reviews/{reviewId}")
 	public ResponseEntity<ApiResponse<Void>> deleteStoreReview(
-		@PathVariable Long storeId,
-		@PathVariable Long reviewId
+		@Parameter(description = "가게 ID") @PathVariable Long storeId,
+		@Parameter(description = "삭제할 리뷰 ID") @PathVariable Long reviewId
 	){
 		storeService.deleteStoreReview(storeId, reviewId);
 		return ResponseEntity.ok(ApiResponse.success());
 	}
 
+	@Operation(summary = "가게 운영시간 수정", description = "가게의 운영시간을 수정합니다.")
 	@PatchMapping("/{storeId}/operatingHours")
 	public ResponseEntity<ApiResponse<Void>> updateOperatingHours(
-		@PathVariable Long storeId,
+		@Parameter(description = "수정할 가게 ID") @PathVariable Long storeId,
 		@RequestBody @Valid UpdateStoreOperatingHoursRequest updateStoreOperatingHoursRequest
 	){
 		storeService.updateStoreOperatingHours(storeId, updateStoreOperatingHoursRequest);
 		return ResponseEntity.ok(ApiResponse.success());
 	}
 
+	@Operation(summary = "가게 리뷰 수정", description = "가게에 달았던 리뷰를 수정합니다.")
 	@PatchMapping("/{storeId}/reviews/{reviewId}")
 	public ResponseEntity<ApiResponse<Void>> updateStoreReview(
-		@PathVariable Long storeId,
-		@PathVariable Long reviewId,
+		@Parameter(description = "수정할 가게 ID") @PathVariable Long storeId,
+		@Parameter(description = "수정할 리뷰 ID")@PathVariable Long reviewId,
 		@RequestBody @Valid UpdateStoreReviewRequest updateStoreReviewRequest
 	){
 		storeService.updateStoreReview(storeId, reviewId, updateStoreReviewRequest);
