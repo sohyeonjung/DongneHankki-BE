2104eea5390ec39dcbbbc5a3a8e5faf4dfd64f5b
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-09-05 17:21:09 +0900

    [feat] #91 춘천 위도, 경도 추가

--- src/main/java/org/netway/dongnehankki/store/application/ChunCheonStoreService.java
+++ src/main/java/org/netway/dongnehankki/store/application/ChunCheonStoreService.java
@@ -2,8 +2,10 @@ package org.netway.dongnehankki.store.application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.netway.dongnehankki.store.domain.Store;
+import org.netway.dongnehankki.store.infrastructure.external.AddressApiClient;
 import org.netway.dongnehankki.store.infrastructure.external.ChunCheonOpenApiClient;
 import org.netway.dongnehankki.store.infrastructure.repository.StoreRepository;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,11 @@ public class ChunCheonStoreService {
 	private final ChunCheonOpenApiClient openApiClient;
 	private final ObjectMapper objectMapper = new ObjectMapper();
 	private final StoreRepository storeRepository;
+	private final AddressApiClient addressApiClient;
+
+	private static final Set<String> VALID_INDU_TYPE_CODES = Set.of(
+		"2502", "2301", "2105", "5201"
+	);
 
 	public void fetchAndSaveStores(int pageIndex, int pageSize) throws Exception {
 		String response = openApiClient.fetchStoreData(pageIndex, pageSize);
@@ -32,23 +39,27 @@ public class ChunCheonStoreService {
 		if (dataNode.isArray()) {
 			for (JsonNode node : dataNode) {
 				String sigun = node.get("시군").asText();
-				if ("춘천시".equals(sigun)) {
-					String name = node.get("업체명").asText();
-					String address = node.get("소재지주소").asText();
+				if(!"춘천시".equals(sigun)) continue;
 
-					//TODO 여기에 주소를 위도/경도로 바꾸는 코드 추가
-					//TODO 업체분류를 industryCode로 변경하는 코드 추가
-					Store store = Store.createStore(
-						name,
-						null,   // latitude (위도)
-						null,   // longitude (경도)
-						address,
-						sigun,
-						null,   // industryCode
-						null    // businessRegistrationNumber
-					);
-					stores.add(store);
-				}
+				String name = node.get("업체명").asText();
+				String address = node.get("소재지주소").asText();
+
+				double[] coords = addressApiClient.getCoordinates(address);
+				Double latitude = coords[0];
+				Double longitude = coords[1];
+
+				//TODO 업체분류를 industryCode로 변경하는 코드 추가
+				Store store = Store.createStore(
+					name,
+					latitude,   // latitude (위도)
+					longitude,   // longitude (경도)
+					address,
+					sigun,
+					2301,   // industryCode
+					null    // businessRegistrationNumber
+				);
+				stores.add(store);
+
 			}
 		}
 
