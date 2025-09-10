7c1b15aab30bed711eb08acd91fe2efb66e59d48
HoyiTT <pjs9177@naver.com>
2025-09-10 12:35:47 +0900

    [refactor]#108 유저 행위 패키지 record -> class로 수정

--- src/main/java/org/netway/dongnehankki/analytics/dto/response/WeeklyHourlyStatsResponse.java
+++ src/main/java/org/netway/dongnehankki/analytics/dto/response/WeeklyHourlyStatsResponse.java
@@ -2,9 +2,13 @@ package org.netway.dongnehankki.analytics.dto.response;
 
 import java.time.DayOfWeek;
 import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-public record WeeklyHourlyStatsResponse(
-    DayOfWeek dayOfWeek,
-    List<HourlyStat> hourlyStats
-) {
+@Getter
+@RequiredArgsConstructor
+public class WeeklyHourlyStatsResponse {
+
+    private final DayOfWeek dayOfWeek;
+    private final List<HourlyStat> hourlyStats;
 }
