9d7f06d2e490380577a341b40b2b401270ff604e
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-09-16 21:37:15 +0900

    [test] #119 schedulingConfig test 변경

--- src/test/java/org/netway/dongnehankki/global/config/ScheduleConfigTest.java
+++ src/test/java/org/netway/dongnehankki/global/config/ScheduleConfigTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.netway.dongnehankki.store.application.StoreChunCheonDataServiceImpl;
+import org.netway.dongnehankki.store.application.StoreChunCheonDataService;
 import org.netway.dongnehankki.store.application.StoreGwangmyeongDataService;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,20 +18,31 @@ class ScheduleConfigTest {
 	private StoreGwangmyeongDataService gwangmyeongDataService;
 
 	@Mock
-	private StoreChunCheonDataServiceImpl chunCheonStoreService;
+	private StoreChunCheonDataService chunCheonStoreService;
 
 	@InjectMocks
 	private ScheduleConfig scheduleConfig;
 
 	@Test
-	@DisplayName("어플리케이션 실행 시 함수 호출")
-	void syncStoreOpenData_shouldCallSyncService() throws Exception {
+	@DisplayName("어플리케이션 시작 시 광명 데이터 동기화 함수 호출")
+	void syncGwangmyeongDataOnStartup_shouldCallGwangmyeongDataService() throws Exception {
 		// When
-		scheduleConfig.onApplicationReady();
+		scheduleConfig.syncGwangmyeongDataOnStartup();
 
 		// Then
 		verify(gwangmyeongDataService, times(1)).saveAllStores(1000);
+		verify(chunCheonStoreService, never()).saveAllStores(anyInt());
+	}
+
+	@Test
+	@DisplayName("서버 시작 후 10분 지연 춘천 데이터 동기화 함수 호출")
+	void syncChunCheonStoreDataOnStartup_shouldCallChunCheonDataService() throws Exception {
+		// When
+		scheduleConfig.syncChunCheonStoreDataOnStartup();
+
+		// Then
 		verify(chunCheonStoreService, times(1)).saveAllStores(1000);
+		verify(gwangmyeongDataService, never()).saveAllStores(anyInt());
 	}
 }
 
